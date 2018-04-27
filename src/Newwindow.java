b163fd26dfdde3821435660a0d28e9cb4bd2a7c1
晓辉 <aclihui@gmail.com>
2018-04-27 08:19:36 +0800

    Initial commit

---
+++ src/Newwindow.java
@@ -0,0 +1,19 @@
+/**
+* 
+* @author LiHui
+* @version 创建时间：2018年4月27日 上午8:17:02
+* @version 0.01
+* @Instruction  Instruction
+*/
+import javax.swing.JFrame;
+public class Newwindow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame win =new JFrame("simple window");
+		win.setSize(300, 200);
+		win.setVisible(true);
+		win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
